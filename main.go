f621bae304e23db2d0ba601e8b05b902d46c1339
Sergiy Chumachenko <chumachenko.sergiy@gmail.com>
2020-09-08 21:23:16 +0300

    printing the clock has been finished

--- main.go
+++ main.go
@@ -1,6 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func timeToDigits(currentTime time.Time) []int {
+	convertedToString := strconv.Itoa(currentTime.Hour()) + strconv.Itoa(currentTime.Minute()) + strconv.Itoa(currentTime.Second())
+	split := strings.Split(convertedToString, "")
+	return convertToDigits(split)
+}
+
+func convertToDigits(strArr []string) []int {
+	var intArr []int
+	for _, v := range strArr {
+		j, err := strconv.Atoi(v)
+		if err != nil {
+			panic(err)
+		}
+		intArr = append(intArr, j)
+
+	}
+	return intArr
+}
 
 func main(){
 	digits := [10][5]string{
@@ -76,11 +100,19 @@ func main(){
 		},
 	}
 
-	for _, v:= range digits{
-		for _, j := range v {
-			fmt.Printf("%s\n", j)
-		}
-		fmt.Printf("\n")
+	separator := [5]string{
+		"   ",
+		" ░ ",
+		"   ",
+		" ░ ",
+		"   ",
 	}
-
+	converted := timeToDigits(time.Now())
+    for i:=0;i<5;i++{
+		fmt.Printf("%s %s %s %s %s %s %s %s\n",
+			digits[converted[0]][i], digits[converted[1]][i], separator[i],
+			digits[converted[2]][i], digits[converted[3]][i], separator[i],
+			digits[converted[4]][i], digits[converted[5]][i],
+		)
+	}
 }
