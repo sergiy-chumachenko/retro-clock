2df15d5e4de783c7d3a755531a8dedf042aee6c4
Sergiy Chumachenko <chumachenko.sergiy@gmail.com>
2020-09-09 21:49:54 +0300

    few improvements have been made

--- main.go
+++ main.go
@@ -3,50 +3,12 @@ package main
 import (
 	"fmt"
 	"github.com/inancgumus/screen"
-	"strconv"
-	"strings"
 	"time"
 )
 
-func timeToDigits(currentTime time.Time) []int {
-	hours, minutes, seconds := strconv.Itoa(currentTime.Hour()), strconv.Itoa(currentTime.Minute()), strconv.Itoa(currentTime.Second())
-	convertedToString := checkLength(hours) + checkLength(minutes) + checkLength(seconds)
-	split := strings.Split(convertedToString, "")
-	return convertToDigits(split)
-}
-
-func checkLength(base string) string {
-	if len(base) < 2 {
-		base = "0" + base
-	}
-	return base
-}
-
-func convertToDigits(strArr []string) []int {
-	var intArr []int
-	for _, v := range strArr {
-		j, err := strconv.Atoi(v)
-		if err != nil {
-			panic(err)
-		}
-		intArr = append(intArr, j)
-
-	}
-	return intArr
-}
-
-func drawTime(convertedTime []int, separator [5]string, digits [10][5]string) {
-	for i := 0; i <= 4; i++ {
-		fmt.Printf("%s %s %s %s %s %s %s %s\n",
-			digits[convertedTime[0]][i], digits[convertedTime[1]][i], separator[i],
-			digits[convertedTime[2]][i], digits[convertedTime[3]][i], separator[i],
-			digits[convertedTime[4]][i], digits[convertedTime[5]][i],
-		)
-	}
-}
-
 func main() {
-	digits := [10][5]string{
+	type separator [5]string
+	digits := [10]separator{
 		{
 			"███",
 			"█ █",
@@ -119,35 +81,37 @@ func main() {
 		},
 	}
 
-	separator := [5]string{
+	sep := separator{
 		"   ",
 		" ░ ",
 		"   ",
 		" ░ ",
 		"   ",
 	}
-	separator2 := [5]string{
-		"   ",
-		"   ",
-		"   ",
-		"   ",
-		"   ",
-	}
-
 	screen.Clear()
-	flag := true
 	for {
 		screen.MoveTopLeft()
-		converted := timeToDigits(time.Now())
-		var sep [5]string
-		if flag {
-			sep = separator
-			flag = false
-		} else {
-			sep = separator2
-			flag = true
+		currentTime := time.Now()
+		hours, minutes, seconds := currentTime.Hour(), currentTime.Minute(), currentTime.Second()
+
+		clock := [...]separator{
+			digits[hours/10], digits[hours%10],
+			sep,
+			digits[minutes/10], digits[minutes%10],
+			sep,
+			digits[seconds/10], digits[seconds%10],
+		}
+		for line := range clock[0] {
+			for index, digit :=range clock {
+				next := clock[index][line]
+				if digit == sep && seconds%2 == 0 {
+					next = "   "
+				}
+				fmt.Printf("%s  ",next)
+			}
+			fmt.Printf("\n")
 		}
 		time.Sleep(time.Second)
-		drawTime(converted, sep, digits)
 	}
+
 }
